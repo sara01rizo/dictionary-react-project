f70eeb472966b3662e5266e99738ed29c9606769
Sara Pulido <sara01rizo@gmail.com>
2021-11-22 12:23:47 +0100

    Project deploy in Netlify - succesfully - add footer span use for external links-

--- src/App.js
+++ src/App.js
@@ -3,5 +3,22 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Dictionary React Project</div>;
+  return (
+    <div className="App">
+      <h1>Dictionary React Project</h1>
+      <footer>
+        {" "}
+        <span>
+          This project was coded by{" "}
+          <a href="#" target="_blank">
+            Sara Pulido
+          </a>{" "}
+          and is open-sourced on{" "}
+          <a href="" target="_blank">
+            GitHub
+          </a>
+        </span>
+      </footer>
+    </div>
+  );
 }
