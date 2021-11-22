df01dd467bf354d5434a381de31536a8d6ae6b61
Sara Pulido <sara01rizo@gmail.com>
2021-11-23 00:30:23 +0100

    Feature: Dictionary component  create and logo load

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import React from "react";
-
+import logo from "./logo.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
+
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Dictionary React Project</h1>
-      <footer>
-        {" "}
-        <span>
-          This project was coded by{" "}
-          <a href="#" target="_blank">
-            Sara Pulido
-          </a>{" "}
-          and is open-sourced on{" "}
-          <a href="" target="_blank">
-            GitHub
-          </a>
-        </span>
-      </footer>
+      <div className="container">
+        <h1>Dictionary React Project</h1>
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <a href="#" className="btn btn-primary">
+          Hello
+        </a>
+        <main>
+          <Dictionary />
+        </main>
+        <footer>
+          {" "}
+          <span>
+            This project was coded by{" "}
+            <a href="#" target="_blank">
+              Sara Pulido
+            </a>{" "}
+            and is open-sourced on{" "}
+            <a href="" target="_blank">
+              GitHub
+            </a>
+          </span>
+        </footer>
+      </div>
     </div>
   );
 }
