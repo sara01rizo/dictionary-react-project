f254f4b47ae53d135964455d0daee212b856a523
Sara Pulido <sara01rizo@gmail.com>
2021-11-29 22:05:23 +0100

    Creat: Phonetic component - return a links for listen the phonetics

--- src/Results.js
+++ src/Results.js
@@ -1,17 +1,24 @@
 import React from "react";
+import Phonetic from "./Phonetic";
 import Meaning from "./Meaning";
 
 export default function Results(props) {
-  // console.log(props.results);
   if (props.results) {
     return (
       <div className="Results">
-        <h2>{props.results.word}</h2>
+        <section>
+          <h2>{props.results.word}</h2>
+          {props.results.phonetics.map((phonetic, index) => (
+            <div key={index}>
+              <Phonetic phonetic={phonetic} />
+            </div>
+          ))}
+        </section>
         {props.results.meanings.map((meaning, index) => (
           <div key={index}>
             <section>
               <Meaning meaning={meaning} />
-            </section>
+            </section>{" "}
           </div>
         ))}
       </div>
