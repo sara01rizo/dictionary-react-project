1f9db2f14fcdc91d9f3cf9c23ec7d502fdfcfc68
Sara Pulido <sara01rizo@gmail.com>
2021-12-02 16:32:12 +0100

    Creat: new component Photos - display 1

--- src/Photos.js
+++ src/Photos.js
@@ -1,9 +1,13 @@
-import React from "react";
-
 export default function Photos(props) {
   if (props.photos) {
     console.log(props.photos);
-    return "hello from Photos";
+    return (
+      <section className="Photos">
+        {props.photos.map(function (photo, index) {
+          return <img src={photo.src.tiny} key={index} alt="photos" />;
+        })}
+      </section>
+    );
   } else {
     return null;
   }
