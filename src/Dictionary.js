29dafecd5414444d1e9185f2e820e39a01a1419c
Sara Pulido <sara01rizo@gmail.com>
2021-12-02 16:49:13 +0100

    Update: function display 6 photos per page thk ApiPexels

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -20,7 +20,7 @@ export default function Dictionary(props) {
     let pexelsApiKey =
       "563492ad6f91700001000001ea034b61aa464d4796b2f64d3fd0f235";
 
-    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=1`;
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=6`;
     let headers = { Authorization: `Bearer ${pexelsApiKey}` };
     axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
--- src/Photos.js
+++ src/Photos.js
@@ -3,9 +3,22 @@ export default function Photos(props) {
     console.log(props.photos);
     return (
       <section className="Photos">
-        {props.photos.map(function (photo, index) {
-          return <img src={photo.src.tiny} key={index} alt="photos" />;
-        })}
+        <div className="row">
+          {props.photos.map(function (photo, index) {
+            return (
+              <div className="col-4" key={index}>
+                <a href={photo.src.original} target="_blank" rel="noreferrer">
+                  <img
+                    src={photo.src.tiny}
+                    alt="Photos"
+                    title="Photos"
+                    className="img-fluid"
+                  />
+                </a>
+              </div>
+            );
+          })}
+        </div>
       </section>
     );
   } else {
